438e77ba0518674cfd1794d6b75be501d75101b8
EDUJOHQ <edujo14@outlook.com>
2026-01-04 19:57:53 -0500

    Primer Commit - Web CarroQhatu

---
+++ js/datos.js
@@ -0,0 +1,27 @@
+const listaAutos = [
+    {
+        id: "Mitsubishi-L200-4x2", // Este ID debe ser único para cada auto
+        nombre: "Mitsubishi L200 4x2",
+        subtitulo: "Pick-up 2023",
+        motor: "2.4L Diesel",
+        km: "75.000",
+        ubicacion: "Arequipa",
+        precioContado: "16,500 USD",
+        anio: "2019",
+        version: "GLS",
+        fotos: ["img/L2002.jpg", "img/sportage1.jpg", "img/audiq5.jpg"] // La primera es la principal
+    },
+    {
+        id: "Kia-Sportage-4x4-2016-mod-2017",
+        nombre: "Kia Sportage 4x4 2016 mod 2017",
+        subtitulo: "SUV Modelo 2017",
+        motor: "2.0L Gasolina",
+        km: "53.000",
+        ubicacion: "Arequipa",
+        precioContado: "61,000 Soles",
+        anio: "2023",
+        transmision: "Mecánica",
+        fotos: ["img/sportage1.jpg","img/L2002.jpg", "img/audiq5.jpg"]
+    }
+    // Agrega aquí los otros 18 siguiendo el mismo formato...
+];
