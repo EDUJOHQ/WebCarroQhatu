799b4d16904df06bd3ef613b0e47ccd4a86a7009
EDUJOHQ <edujo14@outlook.com>
2026-01-05 00:58:37 -0500

    implemetacion pagina detail.html banner - catalogo - formulario

--- js/datos.js
+++ js/datos.js
@@ -6,10 +6,12 @@ const listaAutos = [
         motor: "2.4L Diesel",
         km: "75.000",
         ubicacion: "Arequipa",
-        precioContado: "16,500 USD",
+        precioContado: "$ 16,500 USD",
         anio: "2019",
         version: "GLS",
-        fotos: ["img/L2002.jpg", "img/sportage1.jpg", "img/audiq5.jpg"] // La primera es la principal
+        motor: "2.4L Diesel",
+        transmision: "Automática",
+        fotos: ["img/mitsubishil200.jpg","img/L2002.jpg", "img/L2003.jpg", "img/L2004.jpg"] // La primera es la principal
     },
     {
         id: "Kia-Sportage-4x4-2016-mod-2017",
@@ -18,10 +20,10 @@ const listaAutos = [
         motor: "2.0L Gasolina",
         km: "53.000",
         ubicacion: "Arequipa",
-        precioContado: "61,000 Soles",
+        precioContado: "S/. 61,000 Soles",
         anio: "2023",
         transmision: "Mecánica",
-        fotos: ["img/sportage1.jpg","img/L2002.jpg", "img/audiq5.jpg"]
+        fotos: ["img/sportage1.jpg","img/KIASPORTAGE2.jpg", "img/KIASPORTAGE3.jpg", "img/KIASPORTAGE4.jpg", "img/KIASPORTAGE5.jpg", "img/KIASPORTAGE6.jpg", "img/KIASPORTAGE7.jpg", "img/KIASPORTAGE8.jpg"]
     }
     // Agrega aquí los otros 18 siguiendo el mismo formato...
 ];
